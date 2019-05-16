b0f126eea4987effef858cf8d31931188ef491d4
dhaya-dn <dhayalandn635@gmail.com>
2019-05-16 20:41:36 +0530

    comment1

---
+++ task3.py
@@ -0,0 +1,19 @@
+x=input("enter the  no : ")
+
+y=input("enter the  no : ")
+a= x + y
+b= x - y
+c= x * y
+d= x / y
+
+
+
+e= x % y 
+
+#print(a)
+print("addition of " + str (x) + " and " + str (y) + " is : " + str(a))
+print("subtraction of " + str (x) + " and " + str (y) + " is : " + str(b))
+print("multiplication of " + str (x) + " and " + str (y) + " is : " + str(c))
+print("division of " + str (x) + " and " + str (y) + " is : " + str(d))
+print("modulus of " + str (x) + " and " + str (y) + " is : " + str(e))
+ 
